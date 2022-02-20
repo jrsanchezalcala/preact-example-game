47c51928b0d9ac25fe7df868dc473abfab334f53
Juan Sanchez <ap@debian10.juan>
2022-02-20 11:07:21 +0100

    styles list items

--- src/app/components/molecules/Item.tsx
+++ src/app/components/molecules/Item.tsx
@@ -1,81 +1,91 @@
 import {
-    Fragment,
-    FunctionalComponent,
-    FunctionComponent,
-    h,
-    JSX
-  } from "preact";
-  import register from "preact-custom-element";
-  import {ItemTextProps} from '../molecules/ItemText';
-  import {ItemImageProps} from '../molecules/ItemImage';
-  import {ButtonProps} from '../atoms/Button';
-  import "../molecules/ItemImage";
-  import "../molecules/ItemText";
-  import "../atoms/Button";
- 
+  Fragment,
+  FunctionalComponent,
+  FunctionComponent,
+  h,
+  JSX
+} from "preact";
+import register from "preact-custom-element";
+import { ItemTextProps } from "../molecules/ItemText";
+import { ItemImageProps } from "../molecules/ItemImage";
+import { ButtonProps } from "../atoms/Button";
+import "../molecules/ItemImage";
+import "../molecules/ItemText";
+import "../atoms/Button";
 
-   
-  declare global {
-    namespace preact.createElement.JSX {
-      interface IntrinsicElements {
-        ["x-item-image"]: ItemImageProps;
-        ["x-item-text"]: ItemTextProps;
-        ["x-button"] : ButtonProps;
-      }
+declare global {
+  namespace preact.createElement.JSX {
+    interface IntrinsicElements {
+      ["x-item-image"]: ItemImageProps;
+      ["x-item-text"]: ItemTextProps;
+      ["x-button"]: ButtonProps;
     }
   }
-  
-  export interface Item{
-    name: string
-        displayName: string,
-        playURL: string,
-        image: string,
-        detailURL: string,
-        currencyData: Currency
-        provider : [string]
-        volatile ?: [string]
-  }
+}
 
-  export interface Currency{
-    [key: string]: {
-      noMob ?: boolean,
-      noDesk ?:boolean,
-      minimumStake : number
-    };
-  }
+export interface Item {
+  name: string;
+  displayName: string;
+  playURL: string;
+  image: string;
+  detailURL: string;
+  currencyData: Currency;
+  provider: [string];
+  volatile?: [string];
+}
 
-  export type ItemProps = {
-    item : Item | string
+export interface Currency {
+  [key: string]: {
+    noMob?: boolean;
+    noDesk?: boolean;
+    minimumStake: number;
   };
-  const getPriceText = (item:Item,currency : string ) => {
-    let minimun = item?.currencyData[currency]?.minimumStake ? item?.currencyData[currency]?.minimumStake : null; 
-    return minimun ? `${item.currencyData[currency].minimumStake} ${currency} min Stake` : `Not available in ${currency}`
-  }
-  const handlePlay = (item : Item) => {
+}
 
-  }
-  export const ItemDisplay: FunctionalComponent<ItemProps> = ({
-    item 
-  }): JSX.Element => {
-    
-    const getItem  = () => {
-      return typeof item == "string" ? JSON.parse(item) : item;
-    }
-    let data = getItem();
-    return (
+export type ItemProps = {
+  item: Item | string;
+};
+const getPriceText = (item: Item, currency: string) => {
+  let minimun = item?.currencyData[currency]?.minimumStake
+    ? item?.currencyData[currency]?.minimumStake
+    : null;
+  return minimun
+    ? `${item.currencyData[currency].minimumStake} ${currency} min Stake`
+    : `Not available in ${currency}`;
+};
+const handlePlay = (item: Item) => {};
+export const ItemDisplay: FunctionalComponent<ItemProps> = ({
+  item
+}): JSX.Element => {
+  const getItem = () => {
+    return typeof item == "string" ? JSON.parse(item) : item;
+  };
+  let data = getItem();
+  return (
+    <Fragment>
       <div class="item">
         <div class="item-col">
-            <x-item-image text={data.detailURL} src={data.image} />
-        </div> 
-        <div class="item-col">
-          <x-item-text price={getPriceText(data,"EUR")} provider={data?.provider ? data?.provider.join(", ") : "No provider"} title={data?.displayName || ""} />      
+          <x-item-image text={data.detailURL} src={data.image} />
         </div>
         <div class="item-col">
-          <x-button text={"PLAY"} onClick={() => {handlePlay(data)}}/>
+          <x-item-text
+            price={getPriceText(data, "EUR")}
+            provider={
+              data?.provider ? data?.provider.join(", ") : "No provider"
+            }
+            title={data?.displayName || ""}
+          />
+        </div>
+        <div class="item-col">
+          <x-button
+            text={"PLAY"}
+            onClick={() => {
+              handlePlay(data);
+            }}
+          />
         </div>
       </div>
-    
-    );
-  };
-  register(ItemDisplay, "x-item", ["item"]);
-  
+    </Fragment>
+  );
+};
+register(ItemDisplay, "x-item", ["item"]);
